f7d9904b5a5a29a7f6878c53bcd6f549fb264886
splint3rsec <khalil.lem@protonmail.com>
2023-05-28 23:43:57 +0200

    Cleanup BasicTests imports

--- tests/mytests/src/test/java/BasicTests.java
+++ tests/mytests/src/test/java/BasicTests.java
@@ -1,22 +1,16 @@
 import org.junit.*;
-import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.PageLoadStrategy;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.Select;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import java.util.*;
-import java.time.Duration;
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 import java.net.URL;
 import java.net.MalformedURLException;
